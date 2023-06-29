0d19ede251701c9468165f024ed476df5c70a21a
Alison Ryan <Ryan.alis@northeastern.edu>
2023-06-29 12:48:50 -0400

    Initial Commit

---
+++ RecordRoom/RecordRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordRoomApp.swift
+//  RecordRoom
+//
+//  Created by Alison Ryan on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct RecordRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
